7ea035d2336b04448ab1b9c0ddb194c826c42739
Jaagup Kippar <jaagup@tlu.ee>
2026-02-09 13:45:06 +0000

    prop.test

---
+++ 02_r_prop_test/teine.R
@@ -0,0 +1,5 @@
+print(prop.test(2, 12))
+#kaheteistkümnest õpilasest on kaks poisid
+#mida sellest järeldada võin
+#vastus - kui neid mõnel teisel kursusel 12 õpilase hulgas on
+# 1 - 5, siis pole põhjust mingit eripära otsida
